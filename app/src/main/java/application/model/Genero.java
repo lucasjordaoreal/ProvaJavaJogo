cec91f6a7d231e7b24517f079dbeb1e18e25c9e8
Lucas Jordão <101470104+lucasjordaoreal@users.noreply.github.com>
2023-06-06 13:32:16 -0300

    Usando a base

---
+++ app/src/main/java/application/model/Genero.java
@@ -0,0 +1,41 @@
+package application.model;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="generos")
+public class Genero {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    @OneToMany(mappedBy = "genero")
+    private Set<Livro> livros;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Set<Livro> getLivros() {
+        return livros;
+    }
+    public void setLivros(Set<Livro> livros) {
+        this.livros = livros;
+    }
+}
